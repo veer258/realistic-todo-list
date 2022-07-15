cbf854c78c977f78d888e80a6a06a856e3cc4bac
Veer <vtube258@gmail.com>
2022-07-15 16:29:28 +0530

    log the input values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [thisItem, setThisItem] = useState("")
+
+  console.log(thisItem);
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" onChange={(params) => setThisItem(params.target.value)}/>
         <button>
           <span>Add</span>
         </button>
